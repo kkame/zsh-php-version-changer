72de0bb8cd8a1c8b317e1847cf5a09078904250e
kkame <kkamenet@gmail.com>
2021-02-27 15:08:50 +0900

    update

--- pvc.zsh
+++ pvc.zsh
@@ -1,41 +1,41 @@
 autoload -U add-zsh-hook
-pvm() {
+pvc() {
 
   if [ -f composer.json ]; then
 
-    PVM_VERSION=$(cat composer.json | jq '.require.php' | grep -o -m 1 '[0-9]\.[0-9]' | head -1)
+    PVC_VERSION=$(cat composer.json | jq '.require.php' | grep -o -m 1 '[0-9]\.[0-9]' | head -1)
 
-    if [ "$PVM_VERSION" ]; then
+    if [ "$PVC_VERSION" ]; then
 
       case "$(uname -a)" in
 
       *Darwin*)
 
-        if [[ -f "/usr/local/opt/php@$PVM_VERSION/bin/php" ]]; then
-          export PVM_ORIGIN_PATH=$PATH
+        if [[ -f "/usr/local/opt/php@$PVC_VERSION/bin/php" ]]; then
+          export PVC_ORIGIN_PATH=$PATH
           cat composer.json | jq '"[" + .name + "] " + .type'
-          echo "PHP" $PVM_VERSION "로 임시 전환합니다"
-          export PATH="/usr/local/opt/php@$PVM_VERSION/bin:$PATH"
-          export PATH="/usr/local/opt/php@$PVM_VERSION/sbin:$PATH"
+          echo "PHP" $PVC_VERSION "로 임시 전환합니다"
+          export PATH="/usr/local/opt/php@$PVC_VERSION/bin:$PATH"
+          export PATH="/usr/local/opt/php@$PVC_VERSION/sbin:$PATH"
         else
-          echo "PHP $PVM_VERSION 이 설치되어있지 않습니다"
-          echo "아래의 명령어로 php $PVM_VERSION 을 설치해주세요"
-          echo "brew install php@$PVM_VERSION"
+          echo "PHP $PVC_VERSION 이 설치되어있지 않습니다"
+          echo "아래의 명령어로 php $PVC_VERSION 을 설치해주세요"
+          echo "brew install php@$PVC_VERSION"
         fi
         ;;
 
       *Ubuntu*)
-        if [[ -f /usr/bin/php$PVM_VERSION ]]; then
+        if [ -f /usr/bin/php$PVC_VERSION ]; then
 
-          PVM_ORIGIN_PHP_VERSION=$(php -v | grep -o -m 1 '[0-9]\.[0-9]' | head -1)
+          PVC_ORIGIN_PHP_VERSION=$(php -v | grep -o -m 1 '[0-9]\.[0-9]' | head -1)
 
           cat composer.json | jq '"[" + .name + "] " + .type'
-          echo "PHP" $PVM_VERSION "로 강제 전환합니다"
-          sudo update-alternatives --set php /usr/bin/php$PVM_VERSION
+          echo "PHP" $PVC_VERSION "로 강제 전환합니다"
+          sudo update-alternatives --set php /usr/bin/php$PVC_VERSION
         else
-          echo "PHP $PVM_VERSION 이 설치되어있지 않습니다"
-          echo "아래의 명령어로 php $PVM_VERSION 을 설치해주세요"
-          echo "sudo apt install php$PVM_VERSION"
+          echo "PHP $PVC_VERSION 이 설치되어있지 않습니다"
+          echo "아래의 명령어로 php $PVC_VERSION 을 설치해주세요"
+          echo "sudo apt install php$PVC_VERSION"
         fi
         ;;
 
@@ -48,19 +48,19 @@ pvm() {
     fi
   else
 
-    if [ "$PVM_ORIGIN_PATH" ]; then
+    if [ "$PVC_ORIGIN_PATH" ]; then
       echo "기존 PHP 버전으로 전환합니다"
-      export PATH=$PVM_ORIGIN_PATH
-      unset PVM_ORIGIN_PATH
+      export PATH=$PVC_ORIGIN_PATH
+      unset PVC_ORIGIN_PATH
     fi
 
-    if [ "$PVM_ORIGIN_PHP_VERSION" ]; then
+    if [ "$PVC_ORIGIN_PHP_VERSION" ]; then
       echo "기존 PHP 버전으로 전환합니다"
-      sudo update-alternatives --set php /usr/bin/php$PVM_ORIGIN_PHP_VERSION
-      unset PVM_ORIGIN_PHP_VERSION
+      sudo update-alternatives --set php /usr/bin/php$PVC_ORIGIN_PHP_VERSION
+      unset PVC_ORIGIN_PHP_VERSION
     fi
 
   fi
 }
-add-zsh-hook chpwd pvm
-pvm
+add-zsh-hook chpwd pvc
+pvc
