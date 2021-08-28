5cc747db59fedd88e6fc68bf679ab6f8e11c65f6
kkame <kkamenet@gmail.com>
2021-08-28 14:25:47 +0900

    update for ubuntu

--- pvc.zsh
+++ pvc.zsh
@@ -27,11 +27,20 @@ pvc() {
       *Ubuntu*)
         if [ -f /usr/bin/php$PVC_VERSION ]; then
 
-          PVC_ORIGIN_PHP_VERSION=$(php -v | grep -o -m 1 '[0-9]\.[0-9]' | head -1)
+          export PVC_ORIGIN_PATH=$PATH
+
+          export TEMP_PATH="/tmp/php$PVC_VERSION"
+          if [ ! -d $TEMP_PATH ]; then
+	     mkdir $TEMP_PATH
+          fi
+	
+          if [ ! -f $TEMP_PATH/php ]; then
+            ln -snf /usr/bin/php$PVC_VERSION $TEMP_PATH/php
+          fi
 
           cat composer.json | jq '"[" + .name + "] " + .type'
-          echo "PHP" $PVC_VERSION "로 강제 전환합니다"
-          sudo update-alternatives --set php /usr/bin/php$PVC_VERSION
+          echo "PHP" $PVC_VERSION "로 임시 전환합니다"
+          export PATH="$TEMP_PATH:$PATH"
         else
           echo "PHP $PVC_VERSION 이 설치되어있지 않습니다"
           echo "아래의 명령어로 php $PVC_VERSION 을 설치해주세요"
@@ -54,13 +63,8 @@ pvc() {
       unset PVC_ORIGIN_PATH
     fi
 
-    if [ "$PVC_ORIGIN_PHP_VERSION" ]; then
-      echo "기존 PHP 버전으로 전환합니다"
-      sudo update-alternatives --set php /usr/bin/php$PVC_ORIGIN_PHP_VERSION
-      unset PVC_ORIGIN_PHP_VERSION
-    fi
-
   fi
 }
 add-zsh-hook chpwd pvc
 pvc
+
