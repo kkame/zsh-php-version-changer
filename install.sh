7c24ccbf769386362c56f0ffd7e06305f787003f
KKAME <kkamenet@gmail.com>
2022-05-07 22:30:48 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 if [ -f ~/.zsh-php-version-changer/pvc.zsh ]; then
   echo "already install";
 else
-  git clone git@github.com:kkame/zsh-php-version-changer.git ~/.zsh-php-version-changer
+  git clone https://github.com/kkame/zsh-php-version-changer.git ~/.zsh-php-version-changer
   echo "[ -f ~/.zsh-php-version-changer/pvc.zsh ] && source ~/.zsh-php-version-changer/pvc.zsh" >> ~/.zshrc
 fi
